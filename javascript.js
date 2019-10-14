eedf1c213d81a6e9fa9ee7a781d6e0c265d6bf9e
juan solano <juanpablosolanosuarez@hotmail.com>
2019-10-14 19:38:50 +0200

    draggable

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 let button = document.getElementById("button")
 let input = document.querySelector("#input_form")
 let tasks = document.getElementById("tasks")
@@ -10,14 +12,16 @@ button.addEventListener("click",(event) =>{
   <div class="result">
    <ul>
    <li>${input.value}</li>
+   <i class="fas fa-grip-lines"></i>
    </ul>
-   <button class="deletec"><i class="fa fa-trash"></i></i></button>
+   <button class="deletec"><i class="fa fa-trash"></i></button>
    </div>
    </div>
    `
 )
 let remove = document.querySelectorAll(".deletec")
 let result = document.querySelectorAll(".result")
+
 remove.forEach(items =>{
   items.addEventListener("click",(event)=>{
      result.forEach(element =>{
@@ -27,4 +31,5 @@ remove.forEach(items =>{
      })
    })
   })
+  Sortable.create(tasks)
 })
